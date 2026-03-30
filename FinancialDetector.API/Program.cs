f431940b8f7c6afa47b780e43d4a8cf8a76b84b0
Çiçek <kendi.mail.adresin@gmail.com>
2026-03-30 12:48:43 +0300

    fix: API projesindeki eksik Swagger paketi eklendi ve Program.cs hatalari giderildi

--- FinancialDetector.API/Program.cs
+++ FinancialDetector.API/Program.cs
@@ -1,23 +1,28 @@
+using FinancialDetector.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// API Controller servislerini ekliyoruz
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Veritabanı (AppDbContext) servisini sisteme kaydediyoruz
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// HTTP Request Pipeline yapılandırması
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
